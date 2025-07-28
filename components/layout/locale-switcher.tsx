88dfb94c13bfaf3a4a18a64f55702a94c0544d0e
David Oost <david@newblack.io>
2025-07-29 01:11:58 +0200

    more i18n

---
+++ components/layout/locale-switcher.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Locale, useTranslations } from "next-intl";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ComponentType } from "react";
+import { usePathname, useRouter } from "@/lib/i18n/navigation";
+import { routing } from "@/lib/i18n/routing";
+import { NL, US } from "country-flag-icons/react/3x2";
+
+const localeToFlag: Record<Locale, ComponentType<{ className?: string }>> = {
+  nl: NL,
+  en: US,
+};
+
+export default function LocaleSwitcher({ locale }: { locale: Locale }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const FlagIcon = localeToFlag[locale];
+  const t = useTranslations("languages");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size={"icon"} variant={"secondary"}>
+          <FlagIcon className="rounded-xs size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-4">
+        {routing.locales.map((loc) => {
+          const ItemFlag = localeToFlag[loc];
+          return (
+            <DropdownMenuItem
+              key={loc}
+              onClick={() => router.replace(pathname, { locale: loc })}
+            >
+              <ItemFlag className="rounded-xs size-4" />
+              {t(loc)}
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
